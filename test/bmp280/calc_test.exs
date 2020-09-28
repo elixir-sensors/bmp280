fd4929a14467f154ad33609b31d5b89ec16c6e02
Frank Hunleth <fhunleth@troodon-software.com>
2020-09-28 16:26:22 -0400

    Add unit tests for bmp280

--- test/bmp280/calc_test.exs
+++ test/bmp280/calc_test.exs
@@ -3,6 +3,22 @@ defmodule BMP280.CalcTest do
   alias BMP280.Calc
   doctest BMP280.Calc
 
+  @bmp280_calibration %BMP280.Calibration{
+    dig_P1: 36635,
+    dig_P2: -10696,
+    dig_P3: 3024,
+    dig_P4: 11092,
+    dig_P5: -241,
+    dig_P6: -7,
+    dig_P7: 12300,
+    dig_P8: -12000,
+    dig_P9: 5000,
+    dig_T1: 28189,
+    dig_T2: 26285,
+    dig_T3: 50,
+    has_humidity?: false
+  }
+
   @bme280_calibration %BMP280.Calibration{
     dig_H1: 75,
     dig_H2: 338,
@@ -46,6 +62,16 @@ defmodule BMP280.CalcTest do
     dig_T3: 50,
     has_humidity?: true
   }
+
+  test "bmp280 calculations" do
+    raw = %{raw_temperature: 536_368, raw_pressure: 284_052}
+    measurement = Calc.raw_to_measurement(@bmp280_calibration, 100_000, raw)
+
+    assert_in_delta 26.7460, measurement.temperature_c, 0.0001
+    assert_in_delta 100_391.49, measurement.pressure_pa, 0.01
+    assert measurement.humidity_rh == :unknown
+    assert measurement.dew_point_c == :unknown
+  end
 
   test "bme280 1 calculations" do
     raw = %{raw_temperature: 536_368, raw_pressure: 284_052, raw_humidity: 35181}
--- test/bmp280/calibration_test.exs
+++ test/bmp280/calibration_test.exs
@@ -3,6 +3,29 @@ defmodule BMP280.CalibrationTest do
   alias BMP280.Calibration
   doctest BMP280.Calibration
 
+  test "parse bmp280 1 calibration" do
+    raw_calibration =
+      <<29, 110, 173, 102, 50, 0, 27, 143, 56, 214, 208, 11, 84, 43, 15, 255, 249, 255, 12, 48,
+        32, 209, 136, 19>>
+
+    assert Calibration.from_binary(raw_calibration) ==
+             %BMP280.Calibration{
+               dig_P1: 36635,
+               dig_P2: -10696,
+               dig_P3: 3024,
+               dig_P4: 11092,
+               dig_P5: -241,
+               dig_P6: -7,
+               dig_P7: 12300,
+               dig_P8: -12000,
+               dig_P9: 5000,
+               dig_T1: 28189,
+               dig_T2: 26285,
+               dig_T3: 50,
+               has_humidity?: false
+             }
+  end
+
   test "parse bme280 1 calibration" do
     raw_calibration =
       <<29, 110, 173, 102, 50, 0, 27, 143, 56, 214, 208, 11, 84, 43, 15, 255, 249, 255, 12, 48,
