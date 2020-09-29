5dbea042de222fc9f6f49e5557e9b6b2edc16e02
Frank Hunleth <fhunleth@troodon-software.com>
2020-09-28 22:08:55 -0400

    Remove non-SI conversion helper functions
    There are better ways of implementing these and it's really not core to
    the library, so remove them.

--- lib/bmp280/measurement.ex
+++ lib/bmp280/measurement.ex
@@ -1,9 +1,9 @@
 defmodule BMP280.Measurement do
   @moduledoc """
-  One measurement from the sensor
+  One sensor measurement report
 
-  This module holds one measurement from the sensor in SI units. Helper functions
-  are available non-SI units.
+  The temperature, pressure and relative humidity measurements are computed
+  directly from the sensor. All other values are derived.
   """
   defstruct [:temperature_c, :pressure_pa, :altitude_m, :humidity_rh, :dew_point_c]
 
@@ -14,28 +14,4 @@ defmodule BMP280.Measurement do
           humidity_rh: number() | :unknown,
           dew_point_c: number() | :unknown
         }
-
-  @doc """
-  Return the temperature in the specified units
-  """
-  @spec temperature(t(), :celsius | :fahrenheit) :: number()
-  def temperature(measurement, units \\ :celsius)
-  def temperature(measurement, :celsius), do: measurement.temperature_c
-  def temperature(measurement, :fahrenheit), do: 32 + 1.8 * measurement.temperature_c
-
-  @doc """
-  Return the pressure in the specified units
-  """
-  @spec pressure(t(), :pascal | :in_hg) :: number()
-  def pressure(measurement, units \\ :pascal)
-  def pressure(measurement, :pascal), do: measurement.pressure_pa
-  def pressure(measurement, :in_hg), do: measurement.pressure_pa * 0.00029529983071445
-
-  @doc """
-  Return the estimated altitude
-  """
-  @spec altitude(t(), :meters | :feet) :: number()
-  def altitude(measurement, units \\ :meters)
-  def altitude(measurement, :meters), do: measurement.altitude_m
-  def altitude(measurement, :feet), do: 3.2808399 * measurement.altitude_m
 end
