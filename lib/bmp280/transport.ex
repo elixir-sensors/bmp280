7b3718738cd8daf82329ddf6edf1ca0a10542e32
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2021-02-21 14:21:10 -0500

    Add bus address validation

--- lib/bmp280/transport.ex
+++ lib/bmp280/transport.ex
@@ -1,6 +1,8 @@
 defmodule BMP280.Transport do
   @moduledoc false
 
+  require Logger
+
   alias Circuits.I2C
 
   defstruct [:i2c, :address]
@@ -8,8 +10,19 @@ defmodule BMP280.Transport do
 
   @spec open(String.t(), I2C.address()) :: {:ok, t()} | {:error, any()}
   def open(bus_name, address) do
-    with {:ok, i2c} <- I2C.open(bus_name) do
-      {:ok, %__MODULE__{i2c: i2c, address: address}}
+    if address_exist?(address) do
+      case I2C.open(bus_name) do
+        {:ok, i2c} ->
+          Logger.info("Opened connection to address #{address} on bus #{bus_name}")
+          {:ok, %__MODULE__{i2c: i2c, address: address}}
+
+        error ->
+          Logger.error("Could not connect to address #{address} on bus #{bus_name}")
+          error
+      end
+    else
+      Logger.error("Address #{address} not found")
+      {:error, :invalid_address}
     end
   end
 
@@ -21,5 +34,12 @@ defmodule BMP280.Transport do
   @spec read(t(), 0..255, non_neg_integer()) :: {:ok, binary()} | {:error, any()}
   def read(transport, register, bytes_to_read) do
     I2C.write_read(transport.i2c, transport.address, <<register>>, bytes_to_read)
+  end
+
+  defp address_exist?(address) do
+    case I2C.discover_one([address]) do
+      {:ok, _} -> true
+      _ -> false
+    end
   end
 end
