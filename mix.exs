15f3bae748556948c3336badc2f746f4d82d59d6
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2021-05-09 09:56:42 -0400

    Make readme be a main doc

--- mix.exs
+++ mix.exs
@@ -65,6 +65,7 @@ defmodule BMP280.MixProject do
   defp docs do
     [
       extras: ["README.md", "CHANGELOG.md"],
+      main: "readme",
       source_ref: "v#{@version}",
       source_url: @source_url,
       skip_undefined_reference_warnings_on: ["CHANGELOG.md"]
