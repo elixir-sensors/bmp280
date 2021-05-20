b2a42ccd10c07e1ccf7d377505e764491d9f4d88
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2021-05-20 18:24:00 -0400

    Refactor BME280Comm

--- lib/bmp280/comm/bme280_comm.ex
+++ lib/bmp280/comm/bme280_comm.ex
@@ -1,7 +1,7 @@
 defmodule BMP280.BME280Comm do
   @moduledoc false
 
-  alias BMP280.{BME280Sensor, Transport}
+  alias BMP280.Transport
 
   @calib00_register 0x88
   @calib26_register 0xE1
@@ -9,16 +9,17 @@ defmodule BMP280.BME280Comm do
   @ctrl_meas_register 0xF4
   @press_msb_register 0xF7
 
+  @oversampling_2x 2
+  @oversampling_16x 5
+
+  @normal_mode 3
+
   @spec set_oversampling(Transport.t()) :: :ok | {:error, any()}
   def set_oversampling(transport) do
-    # normal
-    mode = 3
-    # x2 oversampling
-    osrs_t = 2
-    # x16 oversampling
-    osrs_p = 5
-    # x16 oversampling
-    osrs_h = 5
+    mode = @normal_mode
+    osrs_t = @oversampling_2x
+    osrs_p = @oversampling_16x
+    osrs_h = @oversampling_16x
 
     with :ok <- Transport.write(transport, @ctrl_hum_register, <<osrs_h>>) do
       Transport.write(
@@ -29,7 +30,7 @@ defmodule BMP280.BME280Comm do
     end
   end
 
-  @spec read_calibration(Transport.t()) :: {:error, any} | {:ok, binary}
+  @spec read_calibration(Transport.t()) :: {:error, any} | {:ok, <<_::264>>}
   def read_calibration(transport) do
     with {:ok, first_part} <- Transport.read(transport, @calib00_register, 26),
          {:ok, second_part} <- Transport.read(transport, @calib26_register, 7) do
@@ -37,14 +38,8 @@ defmodule BMP280.BME280Comm do
     end
   end
 
-  @spec read_raw_samples(Transport.t()) :: {:error, any} | {:ok, BME280Sensor.raw_samples()}
+  @spec read_raw_samples(Transport.t()) :: {:error, any} | {:ok, <<_::64>>}
   def read_raw_samples(transport) do
-    case Transport.read(transport, @press_msb_register, 8) do
-      {:ok, <<pressure::20, _::4, temp::20, _::4, humidity::16>>} ->
-        {:ok, %{raw_pressure: pressure, raw_temperature: temp, raw_humidity: humidity}}
-
-      {:error, _reason} = error ->
-        error
-    end
+    Transport.read(transport, @press_msb_register, 8)
   end
 end
