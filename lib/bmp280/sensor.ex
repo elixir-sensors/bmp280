12204196a43caba4e589c745c0d8a3eafa3f4d09
Masatoshi Nishiguchi <mnishiguchi@users.noreply.github.com>
2021-05-20 21:05:49 -0400

    Fix references in the hexdocs

--- lib/bmp280/sensor.ex
+++ lib/bmp280/sensor.ex
@@ -1,7 +1,18 @@
 defmodule BMP280.Sensor do
   @moduledoc false
 
-  @callback init(BMP280.state()) :: BMP280.state()
+  @type t :: %{
+          calibration:
+            BMP280.BMP280Calibration.t()
+            | BMP280.BME280Calibration.t()
+            | BMP280.BME680Calibration.t(),
+          last_measurement: BMP280.Measurement.t(),
+          sea_level_pa: number(),
+          sensor_type: BMP280.sensor_type(),
+          transport: BMP280.Transport.t()
+        }
 
-  @callback read(BMP280.state()) :: {:ok, BMP280.Measurement.t()} | {:error, any}
+  @callback init(BMP280.Sensor.t()) :: BMP280.Sensor.t()
+
+  @callback read(BMP280.Sensor.t()) :: {:ok, BMP280.Measurement.t()} | {:error, any}
 end
